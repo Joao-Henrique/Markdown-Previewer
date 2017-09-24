aff36790ad2b56ddb9511e97a42c2c14b99ef4a2
Joao Henrique <jppulicidade@gamail.com>
2017-09-24 23:41:11 +0100

    all working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FormControl from 'react-bootstrap/lib/FormControl'
+const marked = require("marked");
 
 class App extends Component {
+
+  state = {
+    markdown: '\n# This is an h1 tag\n\n## This is an h2 tag\n\n*This text will be italic*\n\n**This text will be bold**\n\n* Item 1'
+  }
+
+  updateMarkdown = (markdown) => {
+    this.setState({markdown});
+  }
+
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Markdown Previewer by João Henrique</h1>
+          <h3>1st FreeCodeCamp DataVisualization Project Using React</h3>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="row col-md-12">
+
+{/*  INPUT CONTENT  /////////////////////*/}
+          <div className="container col-md-6" >
+            <div style={{ paddingLeft:"20%"}}>
+              <h1>Enter text here</h1>
+              <FormControl componentClass="textarea" placeholder="Enter your text here" rows="10"  value={this.state.markdown} onChange= {(event) => this.updateMarkdown(event.target.value)}></FormControl>
+            </div>
+          </div>
+
+{/*  OUTPUT CONTENT  /////////////////////*/}
+          <div className="container col-md-6">
+            <div>
+              <div style={{textAlign:"left", paddingLeft:"20%", paddingTop:"40px"}} dangerouslySetInnerHTML = {{__html: marked(this.state.markdown)}}></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
